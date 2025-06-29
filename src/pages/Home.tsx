df76c2b7ff79427081e3ef14e4819225d91cead0
Deema Talat <111456452+deematalat@users.noreply.github.com>
2025-06-29 13:44:15 +0300

    Added postcss.config.js

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HairvanaUserapp from '../components/hairvana-userapp'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <HairvanaUserapp />
+    </div>
+  )
+}
+
+export default Home
